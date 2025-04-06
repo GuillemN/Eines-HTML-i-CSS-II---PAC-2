9ddd5769e9e9c781112c5e8d05b60e41f76b2c19
guillem <guillemnabagmail.com>
2025-04-06 16:22:48 +0200

    acabat

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,14 +1,4 @@
-/**
- * Import dependencies from node_modules
- * see commented examples below
- */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
-
-/**
- * Write any other JavaScript below
- */
 import * as bootstrap from 'bootstrap';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
